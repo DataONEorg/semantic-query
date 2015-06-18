6f5246251b385689f7899cb553e6b1676f3443fe
SophieHou <SophieHou@users.noreply.github.com>
2015-06-18 11:46:56 -0600

    Update prec-recall-queries_function.R

--- src/prec-recall-queries_function.R
+++ src/prec-recall-queries_function.R
@@ -7,23 +7,32 @@
 # 1) The script assumes the .txt/.csv file's first row is the header row and the first column is the query identifier.
 #    i.e the query fragments start on Row 2
 
-prec_recall_function <- function(queryFragmentFileName, runGroupNumber){
+prec_recall_function <- function(numOfRows, queryFragFilePath, queryFragFileName, runGroupNumber, outputFilePath){
   library(dataone)
 
-  #Initial conditions that might be automated
-  # 1) Setting the testing environment
+  #Setting the Initial Conditions:
+  # 1) Testing environment:
   cn <- CNode("SANDBOX2")
-  # 2) Selecting the number of results we would like to return from SOLR
-  rowsOfResult <- "1000"
-  # 3) Defining the entried for "Run_Group"
-  #runGroup <- "1"
+  #cn <- testEnv
+  
+  # 2) Number of results returned from SOLR:
+  # was: rowsOfResult <- "1000"
+  rowsOfResult <- numOfRows
+  # 3) Query Fragment File Path:
+  # was: ~/dataone/gitcheckout/semantic-query/lib/queries/
+  queryFragmentFileDirectory <- queryFragFilePath
+  # 4) Query Fragment File Name:
+  # was: uc52_query_frags_best_SOLR.csv
+  queryFragmentFileName <- queryFragFileName
+  # 5) Defining the entried for "Run_Group"
+  # was: runGroup <- "1"
   runGroup <- runGroupNumber
 
   #queryFragment <- read.csv(
   #      "./lib/queries/uc52_query_frags_best_SOLR.csv", header = T, sep = ",", stringsAsFactors = F)
   #queryFragmentFilePath <- paste0("./lib/queries/", queryFragmentFileName)
-  queryFragmentFilePath <- paste0("~/dataone/gitcheckout/semantic-query/lib/queries/", queryFragmentFileName)
-  #print(queryFragmentFilePath)
+  queryFragmentFilePath <- paste0(queryFragmentFileDirectory, queryFragmentFileName)
+  print(queryFragmentFilePath)
   queryFragment <- read.csv(queryFragmentFilePath, header = T, sep = ",", stringsAsFactors = F)
   
   # Initialize an output data frame with the proper columns
@@ -44,14 +53,16 @@ prec_recall_function <- function(queryFragmentFileName, runGroupNumber){
           result$WasHit <- TRUE
         
           # Add the result rows to our accumulated data frame
-          df <- rbind(df, result)
+         df <- rbind(df, result)
       }
   }
 
   #print(df)
 
   # Write out the results as a CSV file using a filename linked to the runGroup
-  write.csv(df, paste("~/dataone/gitcheckout/semantic-query/results/query-results-run", runGroup, "out_test.csv", sep = '-'), row.names=F)
-  return()
+  #write.csv(df, paste("~/dataone/gitcheckout/semantic-query/results/query-results-run", runGroup, "out_test.csv", sep = '-'), row.names=F)
+  write.csv(df, paste(outputFilePath, runGroup, "out_test.csv", sep = '-'), row.names=F)
+  
+  outputFileLocation = paste(outputFilePath, runGroup, "out_test.csv", sep = '-')
+  return(outputFileLocation)
 }
-
