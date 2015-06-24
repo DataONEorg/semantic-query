499e563b6d002c0abba3369ad16bb12df7edbcdb
mobb <mob@msi.ucsb.edu>
2015-06-23 20:28:39 -0700

    check corpus script takes input filename from the command line

--- src/check-corpus.sh
+++ src/check-corpus.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+# Script checks if IDs from a corpus list exist in a DataONE repo
 
-# Check if IDs from a corpus list exist in a DataONE repo
+# usage
+USAGE='     USAGE: check-corpus.sh filename_with_ids';
+if [ $# -ne 1 ]; 
+    then echo;echo "$USAGE"; echo;
+    exit;
+fi
+
+
 BASEURL=https://cn-sandbox-2.test.dataone.org/cn/v1/meta/
 
-for ID in `cat ../lib/test_corpus_C_id_list.txt`; do 
+# get the list to check
+filename=$1;
+
+for ID in `cat $filename` ; do
 	#echo "Processing: " ${ID}
 	NOTFOUND=`curl -s ${BASEURL}${ID} | grep -c "NotFound"`
 	if [[ "$NOTFOUND" -gt "0" ]] ; then
