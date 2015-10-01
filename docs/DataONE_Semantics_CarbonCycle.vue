6ff5674e2043dd31874f96c012743e8b53d46879
Matt Jones <gitcode@magisa.org>
2015-10-01 15:23:21 -0800

    Added Mark's flowchart for comparison.

---
+++ docs/DataONE_Semantics_CarbonCycle.vue
@@ -0,0 +1,269 @@
+<!-- Tufts VUE 3.2.2 concept-map (DataONE_Semantics_CarbonCycle.vue) 2015-10-01 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/Applications/VUE.app/Contents/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Thu Oct 01 15:02:56 PDT 2015 by schild on platform Mac OS X 10.10.3 in JVM 1.6.0_65-b14-466.1-11M4716 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built May 23 2013 at 2146 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="DataONE_Semantics_CarbonCycle.vue" created="1443727044122"
+    x="0.0" y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1443736976161" size="16096"
+        spec="/Users/schild/DataONE_Semantics_CarbonCycle.vue" type="1" xsi:type="URLResource">
+        <title>DataONE_Semantics_CarbonCycle.vue</title>
+        <property key="File" value="/Users/schild/DataONE_Semantics_CarbonCycle.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/24e9c26a806fdc99014146554a2165be</URIString>
+    <child ID="13"
+        label=" a groundtruth file for &#xa;carbon flux queries &#xa;on test Corpus C, N=157&#xa;"
+        layerID="1" created="1443727238444" x="547.0" y="14.0"
+        width="218.0" height="64.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <notes>One groundtruthing file indicating %sp;boolean status (T/F) for each the ten queries in UC52, determined for each of the data objects in the test corpus "C" (N=157)%nl;%nl;manually created ; %sp;there will be more groundtruth files</notes>
+        <resource referenceCreated="0" accessAttempted="1443727801968"
+            accessSuccessful="1443727801969"
+            spec="https://github.com/DataONEorg/semantic-query/blob/master/lib/ground_truth/test_corpus_c_groundtruth_carbon_flux_queries.csv"
+            type="2" xsi:type="URLResource">
+            <property key="URL" value="https://github.com/DataONEorg/semantic-query/blob/master/lib/ground_truth/test_corpus_c_groundtruth_carbon_flux_queries.csv"/>
+        </resource>
+        <fillColor>#F2AE45</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c271806fdc9901414655f1abd95f</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="14" label=" Use Case 52&#xa;Design Document" layerID="1"
+        created="1443727431537" x="132.0" y="21.0" width="148.0"
+        height="44.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <notes>The Use Case 52 Design Document describes our overall approach to "Searches for DataONE-hosted data packages that match semantic measurement concepts". %sp;%nl;%nl;This document contains within it, ten (10) sample queries that are focused on the Carbon Cycle concepts. %sp;These 10 queries are initial focus of our %sp;Precision/recall runs</notes>
+        <resource referenceCreated="0" accessAttempted="1443727593386"
+            accessSuccessful="1443727593386"
+            spec="https://github.com/DataONEorg/sem-prov-design/blob/master/docs/use-cases/semantics/use-case-52-Semantic-Search.rst"
+            type="2" xsi:type="URLResource">
+            <property key="URL" value="https://github.com/DataONEorg/sem-prov-design/blob/master/docs/use-cases/semantics/use-case-52-Semantic-Search.rst"/>
+        </resource>
+        <fillColor>#F2AE45</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c271806fdc9901414655eab04766</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="18"
+        label="prescribes creating test Corpora, &#xa;for which relevance for each of &#xa;(10) carbon flux queries must be evaluated&#xa;"
+        layerID="1" created="1443728018999" x="279.5" y="18.383331"
+        width="268.0" height="52.0" strokeWidth="1.0" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c272806fdc990141465598e352a3</URIString>
+        <point1 x="280.0" y="43.493332"/>
+        <point2 x="547.0" y="45.27333"/>
+        <ID1 xsi:type="node">14</ID1>
+        <ID2 xsi:type="node">13</ID2>
+    </child>
+    <child ID="20" label="New Text" layerID="1" created="1443734013674"
+        x="810.0" y="115.0" width="150.0" height="16.0"
+        strokeWidth="0.0" autoSized="false" xsi:type="text">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#000000</textColor>
+        <font>SansSerif-plain-14</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/254ad359806fdc9901414655beb9fcbc</URIString>
+        <richText>&lt;html&gt;
+  &lt;head style="color: #000000" color="#000000"&gt;
+    &lt;style type="text/css"&gt;
+      &lt;!--
+        body { font-family: Arial; margin-left: 0px; margin-top: 0px; color: #000000; margin-right: 0px; margin-bottom: 0px; font-size: 12 }
+        ol { margin-left: 30; font-family: Arial; margin-top: 6; vertical-align: middle; font-size: 12; list-style-position: outside }
+        p { margin-left: 0; margin-top: 0; color: #000000; margin-right: 0; margin-bottom: 0 }
+        ul { margin-left: 30; font-family: Arial; margin-top: 6; vertical-align: middle; font-size: 12; list-style-position: outside }
+      --&gt;
+    &lt;/style&gt;
+    
+  &lt;/head&gt;
+  &lt;body&gt;
+    &lt;p style="color: #000000" color="#000000"&gt;
+      
+    &lt;/p&gt;
+  &lt;/body&gt;
+&lt;/html&gt;
+</richText>
+        <label>New Text</label>
+    </child>
+    <child ID="17"
+        label="informs SOLR parameterization of Carbon Flux queries"
+        layerID="1" created="1443727942312" x="40.75" y="64.5"
+        width="268.0" height="48.0" strokeWidth="1.0" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c272806fdc9901414655f0cb865d</URIString>
+        <point1 x="190.89012" y="65.0"/>
+        <point2 x="158.6099" y="112.0"/>
+        <ID1 xsi:type="node">14</ID1>
+        <ID2 xsi:type="node">6</ID2>
+    </child>
+    <child ID="6" label="uc52_queries_all.csv" layerID="1"
+        created="1443727052574" x="66.0" y="112.0" width="155.0"
+        height="44.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>uc52 stands for Use Case 52%nl;%nl;This file contains SOLR parameterizations for 5 types of SOLR queries, each representing a different tier or approach to %sp;"semantic enhancement" for query resolution. %sp;These 5 types of SOLR queries are run for each of the 10 test queries.%nl;%nl;https://github.com/DataONEorg/sem-prov-design/blob/master/docs/use-cases/semantics/use-case-52-Semantic-Search.rst%nl;%nl;manually created</notes>
+        <resource referenceCreated="0" accessAttempted="1443733810914"
+            accessSuccessful="1443733810914"
+            spec="https://github.com/DataONEorg/semantic-query/blob/master/lib/queries/uc52_queries_all.csv"
+            type="2" xsi:type="URLResource">
+            <property key="URL" value="https://github.com/DataONEorg/semantic-query/blob/master/lib/queries/uc52_queries_all.csv"/>
+        </resource>
+        <fillColor>#C1F780</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c26b806fdc990141465582ed1d9c</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="8" label="CSV_used_by_Rcode" layerID="1"
+        created="1443727053191" x="89.26135" y="155.5" width="109.0"
+        height="48.998047" strokeWidth="1.0" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c26d806fdc9901414655a702238a</URIString>
+        <point1 x="143.625" y="156.0"/>
+        <point2 x="143.89772" y="203.99805"/>
+        <ID1 xsi:type="node">6</ID1>
+        <ID2 xsi:type="node">7</ID2>
+    </child>
+    <child ID="7" label="#1 QUERY: &#xa;R code to run SOLR queries"
+        layerID="1" created="1443727053190" x="48.0" y="204.0"
+        width="192.0" height="36.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <resource referenceCreated="0"
+            spec="https://github.com/DataONEorg/semantic-query/blob/master/src/prec-recall-queries_function.R"
+            type="2" xsi:type="URLResource">
+            <property key="URL" value="https://github.com/DataONEorg/semantic-query/blob/master/src/prec-recall-queries_function.R"/>
+        </resource>
+        <fillColor>#FC938D</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c26d806fdc9901414655f66a482e</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="10" label="produces ResultSet" layerID="1"
+        created="1443727055731" x="94.31174" y="239.5" width="97.0"
+        height="42.0" strokeWidth="1.0" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c26f806fdc99014146555d43c038</URIString>
+        <point1 x="143.44444" y="240.0"/>
+        <point2 x="142.17902" y="281.0"/>
+        <ID1 xsi:type="node">7</ID1>
+        <ID2 xsi:type="node">9</ID2>
+    </child>
+    <child ID="9" label="Example &#xa;ResultSet_summary" layerID="1"
+        created="1443727055730" x="67.0" y="281.0" width="149.0"
+        height="44.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>A new Table Format for the CSV output has not yet been generated.%nl;%nl;The goal is, for the 50 queries (10qs x 5 "semantic" types) to create a %sp;Resultset that contains references to Metadata Objects in DataonONE that satisfy the query, in an XML file returned from SOLR%nl;%nl;Columns should be:%nl;R-Time (R)%nl;D1_node (config)%nl;QueryID (q-file)%nl;QueryType (q-file)%nl;DatasetID (SOLR RS)%nl;RunID</notes>
+        <resource referenceCreated="0" accessAttempted="1443735382775"
+            accessSuccessful="1443735382775"
+            spec="https://github.com/DataONEorg/semantic-query/blob/master/results/query-results-run-1-out_test.csv"
+            type="2" xsi:type="URLResource">
+            <property key="URL" value="https://github.com/DataONEorg/semantic-query/blob/master/results/query-results-run-1-out_test.csv"/>
+        </resource>
+        <fillColor>#30D643</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c26e806fdc9901414655af6a5f00</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="24" label="#2 Filter&amp;Merge" layerID="1"
+        created="1443735723802" x="341.0" y="134.0" width="155.0"
+        height="39.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#FC938D</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/256dfb28806fdc9901414655a9cce33a</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="25" label="uses" layerID="1" created="1443735952719"
+        x="181.76259" y="172.5" width="201.10703" height="109.0"
+        strokeWidth="1.0" autoSized="false" controlCount="0"
+        arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/256dfb29806fdc9901414655e5c72401</URIString>
+        <point1 x="382.3696" y="173.0"/>
+        <point2 x="182.26257" y="281.0"/>
+        <ID1 xsi:type="node">24</ID1>
+        <ID2 xsi:type="node">9</ID2>
+    </child>
+    <layer ID="1" label="Layer 1" created="1443727044127" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/24e9c273806fdc9901414655bd106067</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1443727044121"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/24e9c274806fdc9901414655ec1405a0</URIString>
+            <masterSlide ID="2" created="1443727044147" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/24e9c274806fdc99014146559541082f</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1443727044308" x="341.0" y="175.0"
+                    width="118.0" height="50.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/24e9c277806fdc9901414655d3b14866</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1443727044309" x="349.5" y="282.5"
+                    width="101.0" height="35.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/24e9c278806fdc990141465564136419</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1443727044309"
+                    x="375.5" y="385.0" width="49.0" height="30.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/24e9c278806fdc9901414655170c2b6e</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2015-10-01</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>/Users/schild</saveLocation>
+    <saveFile>/Users/schild/DataONE_Semantics_CarbonCycle.vue</saveFile>
+</LW-MAP>
