28dadccca7391e22be63d29e4d5a677acf4c38b3
SophieHou <SophieHou@users.noreply.github.com>
2015-10-12 14:23:21 -0700

    Update Query_Function.R
    1) Added additional clarification comments to the script.
    2) Modified the resulting data frame, so that it is conforming to the format documented in the following etherpad notes: https://epad.dataone.org/pad/p/2015-10-01-sem-query-notes
    3) Updated the script, so that it could submit multiple queries based on the information provided by the input csv file.

--- src/Query_Function.R
+++ src/Query_Function.R
@@ -1,11 +1,12 @@
 # Purpose: The script will perform the following:
-# 1) Read in a pre-deteremined group of query fragements from a .csv file.
+# 1) Read in a pre-deteremined group of query fragements from a .csv file with the following characteristics:
+#     -- File name: "uc52_queries_all.csv".
+#     -- File Path: Provided by the user as 1 of the 2 input parameters to this function.
+#     -- File Structure: The first row is the header and the order of the header is as follows:
+#       -- Query_ID, SOLR_Index_Type, Query_Frag, Ontology_Set_ID
+#
 # 2) Sequentially submit the query fragements as SOLR queries using the query() function.
 # 3) Accumulate the results in a data frame, and write the results to an output file.
-
-# Note:
-# 1) The script assumes the .csv file's first row is the header and the order of the header is as follows:
-#    Query_ID, SOLR_Index_Type, Query_Frag
 
 library(dataone)
 
@@ -13,6 +14,8 @@ query_function <- function(queryFragFullFilePath, outputFilePath){
 
   
   #Setting the Initial Conditions:
+  # The initial conditions are set using 4 different variables: cn, rowsOfResult, queryFragmentFileDirectory, runGroup
+  
   # 1) Testing environment:
   cn <- CNode("SANDBOX2")
   #print(cn)
@@ -23,51 +26,70 @@ query_function <- function(queryFragFullFilePath, outputFilePath){
   
   # 3) Directory location for the query fragment file:
   # Example: 
+  ## This ecample needs to be commented out during the full automatic test ##
   queryFragFullFilePath <- '~/dataone/gitcheckout/semantic-query/lib/queries/uc52_queries_all_test.csv'
+  
+  
   queryFragmentFileDirectory <- queryFragFullFilePath
   print(queryFragmentFileDirectory)
 
   # 4) Defining the entry for "Run_Group"
-  # Example: runGroup <- "1"
   runGroup <- Sys.time()
   print(runGroup)
   
+  #Read in the query fragments from the input CSV file
   queryFragment <- read.csv(queryFragmentFileDirectory, header = T, sep = ",", quote="", stringsAsFactors = F)
   print(queryFragment)
   
   # Initialize an output data frame with the proper columns
-  df <- data.frame(R_Time = character(0), D1_node = character(0), Query_ID = character(0), SOLR_Index_Type = character(0), Run_ID = character(0), Ontology_Set_ID = character(0), Dataset_ID = character(0), 
+  df <- data.frame(R_Time = character(0), D1_node = character(0), Query_ID = character(0), SOLR_Index_Type = character(0), Run_ID = character(0), Ontology_Set_ID = character(0), 
                    stringsAsFactors = FALSE)
   
-  #for (n in 1:nrow(queryFragment)) {
+  #For each query fragment in the CSV file, submit the query fragment to SOLR and record up to 1000 returned results as a data frame
   
-    n <- 1
+  for (n in 1:nrow(queryFragment)) {
+  
+    #n <- 2
     
     queryid <- queryFragment[n,1]
     querytype <- queryFragment[n,2]
     queryString <- queryFragment[n, 3]
+    queryOntology <- queryFragment[n, 4]
     print(paste0("Running query (", queryid, "): ", queryString))
     
     queryParamList <-
       list(q = queryString, rows = as.character(rowsOfResult), fl = "id")
     
-    str(cn)
+    str(queryParamList)
+    
+    #str(cn)
     
     # Execute the query, and add in the classification columns
-    result <- query(cn, queryParamList, as = "data.frame", parse = FALSE)
+    result <- query(cn, queryParamList, as = "data.frame", parse = TRUE)
     str(result)
     
     if (!is.null(result) & length(result) > 0) {
-      result$Run_ID <- runGroup
+      result$R_Time <- Sys.Date()
+      result$D1_node <- "CN-Sanbox2"
       result$Query_ID <- queryid
+      result$SOLR_Index_Type <- querytype
+      result$Run_ID <- runGroup
+      result$Ontology_Set_ID <- queryOntology
       
       # Add the result rows to our accumulated data frame
       df <- rbind(df, result)
+      
     }
-  #}
+  }
+  
+  #Change the first column header from "id" to "Dataset_ID"
+  names(df)[1] <- "Dataset_ID"
+  
+  # Example: 
+  ## This ecample needs to be commented out during the full automatic test ##
   
   # Write out the results as a CSV file using a filename linked to the runGroup
-  write.csv(df, paste(outputFilePath, runGroup, "out_test.csv", sep = '-'), row.names=F)
+  write.csv(df, paste(outputFilePath, runGroup, "Resultset_Summary.csv", sep = '_'), row.names=F)
   
   outputFileLocation = paste(outputFilePath, runGroup, "Resultset_Summary.csv", sep = '_')
   return(outputFileLocation)
