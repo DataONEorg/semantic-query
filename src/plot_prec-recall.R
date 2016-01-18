0bab66b5477c6bf9d4fcee50c946b454f06f5af5
Matt Jones <gitcode@magisa.org>
2016-01-18 14:35:06 -0900

    Adjust horizontal jitter width, eliminate vertical jitter.

--- src/plot_prec-recall.R
+++ src/plot_prec-recall.R
@@ -9,13 +9,13 @@ plot_pr <- function() {
     p <- ggplot(pr, aes(SOLR_Index_Type, Precision)) + 
         stat_summary(fun.y = mean, geom = "bar", color="black", fill="white") + 
         stat_summary(fun.data = mean_se, geom = "errorbar", width=0.5) + 
-        geom_jitter(color="red")
+        geom_jitter(color="red", height=0, width=0.9)
     ggsave("pr-plot-precision.pdf", plot=p, width=12, height=12)
     
     # Recall
     r <- ggplot(pr, aes(SOLR_Index_Type, Recall)) + 
         stat_summary(fun.y = mean, geom = "bar", color="black", fill="white") + 
         stat_summary(fun.data = mean_se, geom = "errorbar", width=0.5) +
-        geom_jitter(color="red")
+        geom_jitter(color="red", height=0, width=0.9)
     ggsave("pr-plot-recall.pdf", plot=r, width=12, height=12)
 }
