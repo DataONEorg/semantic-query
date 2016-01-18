3926ee285b80d4dfc5ae46c957708609591d7630
Matt Jones <gitcode@magisa.org>
2016-01-18 13:57:50 -0900

    Cleaned up plot aesthetics.

--- src/plot_prec-recall.R
+++ src/plot_prec-recall.R
@@ -7,16 +7,15 @@ plot_pr <- function() {
     
     # Precision
     p <- ggplot(pr, aes(SOLR_Index_Type, Precision)) + 
-        stat_summary(fun.y = mean, geom = "bar") + 
-        stat_summary(fun.data = mean_se, geom = "errorbar") + 
+        stat_summary(fun.y = mean, geom = "bar", color="black", fill="white") + 
+        stat_summary(fun.data = mean_se, geom = "errorbar", width=0.5) + 
         geom_jitter(color="red")
     ggsave("pr-plot-precision.pdf", plot=p, width=12, height=12)
     
     # Recall
     r <- ggplot(pr, aes(SOLR_Index_Type, Recall)) + 
-        stat_summary(fun.y = mean, geom = "bar", color="white") + 
-        stat_summary(fun.data = mean_se, geom = "errorbar") +
+        stat_summary(fun.y = mean, geom = "bar", color="black", fill="white") + 
+        stat_summary(fun.data = mean_se, geom = "errorbar", width=0.5) +
         geom_jitter(color="red")
-    r
     ggsave("pr-plot-recall.pdf", plot=r, width=12, height=12)
 }
