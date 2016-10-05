5f09eb65e30c59d6eaea0d3f43bc2c841243744d
mobb <margaret.obrien@ucsb.edu>
2016-10-05 15:46:41 -0700

    plots include: a) red dots with individual results, b) summary bar for nat_lang. Reduced font size for labels so they fit

--- src/plot_prec-recall.R
+++ src/plot_prec-recall.R
@@ -7,8 +7,8 @@ plot_pr <- function(results_Prec_Recall) {
     pr$Precision <- as.numeric(pr$Precision)
     pr$Recall <- as.numeric(pr$Recall)
     # Use a factor to set the order of search types to be plotted
-    pr$SOLR_Index_Type2 <- factor(pr$SOLR_Index_Type, levels=c("full_text", "metacat_ui", "bioportal_annot", "esor_annot", "manual_annot", "esor_cosine"), 
-                                  labels=c("Full Text", "Structured", "BioPortal", "ESOR", "Manual", "ESOR Cosine"))
+    pr$SOLR_Index_Type2 <- factor(pr$SOLR_Index_Type, levels=c("full_text", "metacat_ui", "bioportal_annot", "esor_annot", 'esor_cosine', "manual_annot", "nat_lang"), 
+                                  labels=c("Full Text", "Structured", "BioPortal", "ESOR wiki", "ESOR cosine", "Manual", "Natural"))
     # Eliminate search types not set in the above factor
     pr <- pr[which(!is.na(pr$SOLR_Index_Type2)), ]
     
@@ -16,21 +16,21 @@ plot_pr <- function(results_Prec_Recall) {
     p <- ggplot(pr, aes(SOLR_Index_Type2, Precision)) + 
         stat_summary(fun.y = mean, geom = "bar", color="black", fill="white") + 
         stat_summary(fun.data = mean_se, geom = "errorbar", width=0.4) + 
-        #geom_jitter(color="red", height=0, width=0.6, size=3) +
+        geom_jitter(color="red", height=0, width=0.6, size=3) +
         scale_y_continuous(limits = c(0, 100)) +
         labs(x="Search Type",y="Precision (%)") +
         theme(axis.title = element_text(face="bold", size=28)) +
-        theme(axis.text = element_text(face="bold", size=24))
+        theme(axis.text = element_text(face="bold", size=18))
     ggsave("pr-plot-precision.pdf", plot=p, width=12, height=12)
     
     # Recall plot
     r <- ggplot(pr, aes(SOLR_Index_Type2, Recall)) + 
         stat_summary(fun.y = mean, geom = "bar", color="black", fill="white") + 
         stat_summary(fun.data = mean_se, geom = "errorbar", width=0.4) +
-        #geom_jitter(color="red", height=0, width=0.6, size=3) +
+        geom_jitter(color="red", height=0, width=0.6, size=3) +
         scale_y_continuous(limits = c(0, 100)) +
         labs(x="Search Type",y="Recall (%)") +
         theme(axis.title = element_text(face="bold", size=28)) +
-        theme(axis.text = element_text(face="bold", size=24))
+        theme(axis.text = element_text(face="bold", size=18))
     ggsave("pr-plot-recall.pdf", plot=r, width=12, height=12)
 }
