d01448583c03841c75edf321faa52f9b27fcd943
Bryce Mecum <petridish@gmail.com>
2016-11-28 18:09:12 -0900

    Clean up query function
    Indenting confused git again so this is another mega commit. All I did
    here was clean things up a tad.

--- src/Query_Function.R
+++ src/Query_Function.R
@@ -12,118 +12,76 @@
 # 1) The output of function is set up to be written to the following directory: '/semantic-query/results/Resultset_Summary'
 
 query_function <- function(queryFragFullFilePath){
-  
-  library(dataone)
-  
-  #Setting the Initial Conditions:
-  # The initial conditions are set using 4 different variables: cn, rowsOfResult, queryFragmentFileDirectory, runGroup
-  
-  # 1) Testing environment:
-  cn <- CNode("SANDBOX2")
-  #print(cn)
-  
-  # 2) Number of results requested from SOLR:
-  rowsOfResult <- 1000
-  #print(rowsOfResult)
-  
-  # 3) Directory location for the query fragment file:
-  # Example: 
-  ## This example needs to be commented out during the full automatic test ##
-  #queryFragFullFilePath <- '~/dataone/gitcheckout/semantic-query/lib/queries/uc52_queries_all.csv'
-  
-  
-  # Commented out because this line of code is unncessary
-  #queryFragmentFileDirectory <- queryFragFullFilePath
-  #print(queryFragmentFileDirectory)
-  
-  # 4) Defining the entry for "Run_Group"
-  runGroup <- Sys.time()
-  #print(runGroup)
-  
-  #Read in the query fragments from the input CSV file
-  queryFragment <- read.csv(queryFragFullFilePath, quote = "", stringsAsFactors = FALSE)
-  stopifnot(nrow(queryFragment) > 0)
-  stopifnot(all(c("Query_ID", 
-                  "SOLR_Index_Type", 
-                  "Query_Frag", 
-                  "Ontology_Set_ID") %in% names(queryFragment)))
-  
-  #print(queryFragment)
-  
-  # Initialize an output data frame with the proper columns
-  df <- data.frame(R_Time = character(0), D1_node = character(0), Query_ID = character(0), SOLR_Index_Type = character(0), Run_ID = character(0), Ontology_Set_ID = character(0), 
-                   stringsAsFactors = FALSE)
-  
-  #For each query fragment in the CSV file, submit the query fragment to SOLR and record up to 1000 returned results as a data frame
-  
-  for (n in 1:nrow(queryFragment)) {
     
-    #n <- 2
+    library(dataone)
     
-    queryid <- queryFragment[n,1]
-    querytype <- queryFragment[n,2]
-    queryString <- queryFragment[n, 3]
-    queryOntology <- queryFragment[n, 4]
-    #print(paste0("Running query (", queryid, "): ", queryString))
+    # Get a CNode instance set up for cn-sandbox-2
+    cn <- CNode("SANDBOX2")
     
-    queryParamList <-
-      list(q = queryString, rows = as.character(rowsOfResult), fl = "id")
+    # 2) Number of results requested from SOLR:
+    rowsOfResult <- 1000
     
-    #str(queryParamList)
+    # Generate a run identifier using the current UTC time. We'll use this later
+    # on when we save this identifier with our results
+    systime <- Sys.time()
+    attr(systime, "tzone") <- "UTC"
+    runGroup <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S_Z")
     
-    # Execute the query, and add in the classification columns
-    query_all_pages <- function(cn, params) {
-        if (!("rows" %in% params)) params[["rows"]] <- "1000"
-        if (!("fl" %in% params)) params[["fl"]] <- "id"
-        params[["start"]] <- "0"
+    #Read in the query fragments from the input CSV file
+    queryFragment <- read.csv(queryFragFullFilePath, quote = "", stringsAsFactors = FALSE)
+    stopifnot(nrow(queryFragment) > 0)
+    stopifnot(all(c("Query_ID", 
+                    "SOLR_Index_Type", 
+                    "Query_Frag", 
+                    "Ontology_Set_ID") %in% names(queryFragment)))
+    
+    
+    # Set up a data frame for later rbinding of individual query results
+    results <- data.frame()
+    
+    for (n in seq_len(nrow(queryFragment))) {
+        queryid <- queryFragment[n,"Query_ID"]
+        querytype <- queryFragment[n,"SOLR_Index_Type"]
+        queryString <- queryFragment[n, "Query_Frag"]
+        queryOntology <- queryFragment[n, "Ontology_Set_ID"]
         
-        # print(params)
-        result <- query(cn, params, as = "data.frame", parse = TRUE)
-        all_pages <- result
+        queryParamList <- list(q = queryString, rows = as.character(rowsOfResult), fl = "id")
         
-        while(nrow(result) >= 1000) {
-            params[["start"]] <- as.character(as.numeric(params[["start"]]) + as.numeric(params[["rows"]]))
-            # print(params)
-            result <- query(cn, params, as = "data.frame", parse = TRUE)
-            all_pages <- rbind(all_pages, result)
+        # Append filter to only find documents in test corpus F
+        queryParamList[["q"]] <- paste0(queryParamList[["q"]], "+test_corpus_sm:F")
+        
+        # Run the query
+        result <- query(cn, queryParamList, as = "data.frame", parse = TRUE)
+        
+        # Stop if there may be truncation of the results
+        if (nrow(result) >= rowsOfResult) {
+            stop(paste0("The maximum number of rows were returned in the last query,\n\n\t",
+                        queryString, "\n\nIt's possible the results were truncated and this will impact the calculation of precision and recall."))
         }
         
-        all_pages
-    # Stop if there may be truncation of the results
-    if (nrow(result) >= rowsOfResult) {
-        stop(paste0("The maximum number of rows were returned in the last query,\n\n\t",
-                    queryString, "\n\nIt's possible the results were truncated and this will impact the calculation of precision and recall."))
+        if (!is.null(result) & length(result) > 0) {
+            result$R_Time <- Sys.Date()
+            result$D1_node <- "urn:node:cnSandbox2"
+            result$Query_ID <- queryid
+            result$SOLR_Index_Type <- querytype
+            result$Run_ID <- runGroup
+            result$Ontology_Set_ID <- queryOntology
+            
+            # Add the result rows to our accumulated data frame
+            results <- rbind(results, result)
+        }
     }
     
-    result <- query_all_pages(cn, queryParamList)
-    #str(result)
+    #Change the first column header from "id" to "Dataset_ID"
+    names(results)[1] <- "Dataset_ID"
     
-    if (!is.null(result) & length(result) > 0) {
-      result$R_Time <- Sys.Date()
-      result$D1_node <- "urn:node:cnSandbox2"
-      result$Query_ID <- queryid
-      result$SOLR_Index_Type <- querytype
-      result$Run_ID <- runGroup
-      result$Ontology_Set_ID <- queryOntology
-      
-      # Add the result rows to our accumulated data frame
-      df <- rbind(df, result)
-      
-    }
-  }
-  
-  #Change the first column header from "id" to "Dataset_ID"
-  names(df)[1] <- "Dataset_ID"
-  
-  #outputFilePath <- '~/dataone/gitcheckout/semantic-query/results/Resultset_Summary'
-  outputFilePath <- "results/Resultset_Summary"
-  
-  # Write out the results as a CSV file using a filename linked to the runGroup
-  outputFileLocation <- paste(outputFilePath, runGroup, ".csv", sep = '_')
-  write.csv(results, outputFileLocation, row.names = FALSE)
-  
-  print("The query results can be found in the following file:")
-  print(outputFileLocation)
-  
-  return(outputFileLocation)
+    outputFilePath <- "results/Resultset_Summary"
+    
+    # Write out the results as a CSV file using a filename linked to the runGroup
+    outputFileLocation <- paste(outputFilePath, runGroup, ".csv", sep = '_')
+    write.csv(results, outputFileLocation, row.names = FALSE)
+    
+    print(paste0("The query results can be found at ", outputFileLocation))
+
+    return(outputFileLocation)
 }
